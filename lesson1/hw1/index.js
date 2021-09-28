4de94ea45f63f72a1b7650a8f5887e8c1de0b7f0
xzibbitt <tyrawitesse@gmail.com>
2021-09-28 22:08:02 +0300

    commit3

--- lesson1/hw1/index.js
+++ lesson1/hw1/index.js
@@ -1,37 +1,46 @@
-console.log ("Hello World")
+console.log("Hello World");
 
-const year = 2021 //number
-let catName = "Till" //string
-let isRain = true //boolean
-let typeHouse = null //null
-let god //undefined
-let nubmerPi = 314159265358979323846n //bigint
+const year = 2021; //number
+let catName = "Till"; //string
+let isRain = true; //boolean
+let typeHouse = null; //null
+let god; //undefined
+let nubmerPi = 314159265358979323846n; //bigint
 
-console.log (year)
-console.log (catName)
-console.log (isRain)
-console.log (typeHouse)
-console.log (god)
-console.log (nubmerPi)
+console.log(year);
+console.log(catName);
+console.log(isRain);
+console.log(typeHouse);
+console.log(god);
+console.log(nubmerPi);
 
-console.log (typeof year)
-console.log (typeof catName)
-console.log(typeof null)
+console.log(typeof year);
+console.log(typeof catName);
+console.log(typeof null);
 
 const human = {
-    legs: 2,
-    eyes: 2
-}
-console.log(human)
+  legs: 2,
+  eyes: 2,
+};
+console.log(human);
 
-human.eyes = 3
-console.log(human.eyes)
+human.eyes = 3;
+console.log(human.eyes);
 
 let pirate = {
-    legs: 2,
-    eyes: 2
-}
-console.log(pirate)
+  legs: 2,
+  eyes: 2,
+};
+console.log(pirate);
 
-pirate.eyes = 1
-console.log(pirate.eyes)
+pirate.eyes = 1;
+console.log(pirate.eyes);
+
+var angel = {
+  wings: 2,
+  legs: 2,
+};
+console.log(angel);
+
+angel.wings = 0;
+console.log(angel.wings);
