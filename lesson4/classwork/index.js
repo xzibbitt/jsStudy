b0d7da3a6920d2caf7f8220f883c5cd317591b8b
xzibbitt <tyrawitesse@gmail.com>
2021-10-13 23:37:29 +0300

    homework4

---
+++ lesson4/classwork/index.js
@@ -0,0 +1,186 @@
+// if (1) {
+//   console.log("true");
+// }
+
+// if (0) {
+//   console.log("false");
+// }
+// console.log("start");
+
+// const value = -5;
+
+// if (value > 0) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// console.log("finish");
+
+// console.log("start");
+
+// const value = 7;
+// if (value === 0) {
+//   console.log(0, "=value");
+// } else if (value > 1) {
+//   console.log(value, " >1");
+// } else if (value < -1) {
+//   console.log(value, " <1");
+// } else {
+//   console.log(value);
+// }
+
+// console.log("finish");
+
+// const age = 12;
+// const result = age >= 18 ? "доступ разрешен" : "доступ запрещен";
+
+// console.log(result);
+
+// switch ("ivan") {
+//   case "ivan":
+//     console.log("ivan");
+//     break;
+//   case "alex":
+//     console.log("alex");
+//     break;
+//   case "max":
+//     console.log("max");
+//     break;
+//   default:
+//     console.log("имя нет в списке");
+// }
+
+// console.log("finish");
+
+// let a = 2;
+// let b = 66;
+// let c = 35;
+// if (a > b) {
+//   console.log(a);
+// } else if (a > c) {
+//   console.log(a);
+// } else if (b > c) {
+//   console.log(b);
+// } else {
+//   console.log(c);
+// }
+// console.log("finish");
+
+const temp = 49;
+if (temp < -30) {
+  console.log(temp, "оставайтесь дома!");
+} else if (temp < -10) {
+  console.log(temp, "сегодня холодно");
+} else if (temp < 5) {
+  console.log(temp, "Не холодно");
+} else if (temp < 15) {
+  console.log(temp, "тепло");
+} else if (temp < 25) {
+  console.log(temp, "очень тепло");
+} else if (temp < 35) {
+  console.log(temp, "жарко");
+} else {
+  console.log(temp, "пекло");
+}
+
+// let a = 2;
+// let b = 66;
+// let c = 35;
+// if (a > b) {
+//   a > c ? console.log(a) : console.log(c);
+// } else {
+//   b > c ? console.log(b) : console.log(c);
+// }
+
+// const typeUser = "ingineer";
+
+// switch (typeUser) {
+//   case "user":
+//     console.log("информация о юзере");
+//     break;
+//   case "manager":
+//     console.log("информация о мэнеджере");
+//     break;
+//   case "admin":
+//     console.log("информация об админе");
+//     break;
+//   default:
+//     console.log("no info");
+// }
+
+// const a = 0;
+// const b = 1;
+
+// const result1 = a || b;
+// console.log(result1);
+
+// const a = 10;
+// const b = 1;
+
+// const result1 = a && b;
+// console.log(result1);
+
+// let a = 2;
+// let b = 6;
+// let c = 35;
+// let d = 36;
+
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > a && b > c) {
+//   console.log(b);
+// } else {
+//   console.log(c);
+// }
+
+// console.log((a && b) || (c && d));
+
+// console.log(!!"alex");
+
+// console.log("start");
+// let i = 0;
+// while (true) {
+//   console.log("loop");
+//   i += 1;
+//   if (i > 10) {
+//     console.log("stop");
+//   }
+//   break;
+// }
+
+// console.log("end");
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i += 1;
+// }
+// console.log("end");
+
+// do {
+//   console.log("work");
+// } while (i);
+// console.log("end");
+
+// let i = 0;
+// while (i < 11) {
+//   console.log("type");
+//   i += 1;
+// }
+
+// for (let i = 0; i < 11; i++) {
+//   console.log("type");
+// }
+
+// let n = 9;
+// let m = 3;
+// let days = 0;
+// // for (n; n < m; n / 2) {
+// //   console.log("loop");
+// // }
+// while (n >= m) {
+//   n /= 2;
+//   days += 1;
+// }
+// console.log(days);
